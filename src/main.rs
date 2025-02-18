95a57b52682623b56bc2ffe26d70300060a08053
Azosion <neurosxboy@gmail.com>
2025-02-18 13:40:37 -0800

    Made minor optimizations

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,59 @@
-
+use chrono::{DateTime, Datelike, Utc};
 use fancy_print::{Animation, FancyPrinter};
-use chrono::{Utc, DateTime, Datelike};
 use std::time::{Duration, SystemTime};
 
-
 fn main() {
-   let curr_time = SystemTime::now();
+    let curr_time = SystemTime::now();
 
-   let datetime: DateTime<Utc> = curr_time.into();
+    let datetime: DateTime<Utc> = curr_time.into();
 
-   let weekdate = datetime.date_naive().weekday().to_string();
+    let weekdate = datetime.date_naive().weekday().to_string();
 
-   let printer = FancyPrinter::builder()
-     .animation(Animation::Typing)
-     .time_delay(Duration::from_millis(20))
-     .build();
+    let printer = FancyPrinter::builder()
+        .animation(Animation::Typing)
+        .time_delay(Duration::from_millis(20))
+        .build();
 
-   if weekdate == "Mon" {
-          println!(""); //I did it this way for readabliity, for me.
-          printer.print("   5 days till the weekend, good luck...");        
-          printer.print("---------------------------------------------");
-          printer.print("|> Mon <    Tue      Wed      Thu      Fri  |");
-          println!("");
-   }
+    println!(""); //I did it this way to make it easier to read for me. 
 
-   if weekdate == "Tue" {
-          println!("");
-          printer.print("    4 days till the weekend, keep going!");
-          printer.print("---------------------------------------------");
-          printer.print("|  Mon    > Tue <    Wed      Thu      Fri  |");
-          println!("");
-          
-   }
+    if weekdate == "Mon" {
+         
+        printer.print("   5 days till the weekend, good luck...");
+        printer.print("---------------------------------------------");
+        printer.print("|> Mon <    Tue      Wed      Thu      Fri  |");
+    }
 
-   if weekdate == "Wed" {
-          println!("");
-          printer.print("   3 days till the weekend, half way there!");
-          printer.print("---------------------------------------------");
-          printer.print("|  Mon      Tue    > Wed <    Thu      Fri  |");
-          println!("");
-   }
+    if weekdate == "Tue" {
+        printer.print("    4 days till the weekend, keep going!");
+        printer.print("---------------------------------------------");
+        printer.print("|  Mon    > Tue <    Wed      Thu      Fri  |");
+    }
 
-   if weekdate == "Thu" {
-          println!("");
-          printer.print("   2 days till the weekend, almost there");
-          printer.print("---------------------------------------------");
-          printer.print("|  Mon      Tue      Wed    > Thu <    Fri  |");
-          println!("");
-   }
+    if weekdate == "Wed" {
+        
+        printer.print("   3 days till the weekend, half way there!");
+        printer.print("---------------------------------------------");
+        printer.print("|  Mon      Tue    > Wed <    Thu      Fri  |");
+    }
 
-   if weekdate == "Fri" {
-          println!("");
-          printer.print("1 day till the weekend, last day... u got this!");
-          printer.print("---------------------------------------------");
-          printer.print("|  Mon      Tue      Wed      Thu    > Fri <|");
-          println!("");
-   }
+    if weekdate == "Thu" {
+        
+        printer.print("   2 days till the weekend, almost there");
+        printer.print("---------------------------------------------");
+        printer.print("|  Mon      Tue      Wed    > Thu <    Fri  |");
+    }
 
-   if weekdate == "Sat" || weekdate == "Sun" {
-       println!("");
-       printer.print("It's the weekend, you made it!");
-       println!("");
-   }
+    if weekdate == "Fri" {
+        
+        printer.print("1 day till the weekend, last day... u got this!");
+        printer.print("---------------------------------------------");
+        printer.print("|  Mon      Tue      Wed      Thu    > Fri <|");
+    }
 
+    if weekdate == "Sat" || weekdate == "Sun" {
+        
+        printer.print("It's the weekend, you made it!");
+        
+    }
+    println!("");
 }
