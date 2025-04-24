7918f1d074603150a25b928394e9116b21cef866
azosion <neurosxboy@gmail.com>
2025-04-24 10:28:50 -0700

    adjusted lines to fit cmd output.

--- src/main.rs
+++ src/main.rs
@@ -21,34 +21,34 @@ fn main() {
         if weekdate == "Mon" {
                 //I've separated the lines into 3 prints so that it lines up here, I could turn it all into one print tho.
             println!("   5 days till the weekend, good luck...");
-            println!("---------------------------------------------");
+            println!("---------------------------------------------------");
             println!("|> Mon <    Tue      Wed      Thu      Fri  |");
         }
 
         if weekdate == "Tue" {
             println!("    4 days till the weekend, keep going!");
-            println!("---------------------------------------------");
+            println!("---------------------------------------------------");
             println!("|  Mon    > Tue <    Wed      Thu      Fri  |");
         }
 
         if weekdate == "Wed" {
             
             println!("   3 days till the weekend, half way there!");
-            println!("---------------------------------------------");
+            println!("---------------------------------------------------");
             println!("|  Mon      Tue    > Wed <    Thu      Fri  |");
         }
 
         if weekdate == "Thu" {
             
             println!("    2 days till the weekend, almost there");
-            println!("---------------------------------------------");
+            println!("---------------------------------------------------");
             println!("|  Mon      Tue      Wed    > Thu <    Fri  |");
         }
 
         if weekdate == "Fri" {
             
             println!("1 day till the weekend, last day, u got this!");
-            println!("---------------------------------------------");
+            println!("---------------------------------------------------");
             println!("|  Mon      Tue      Wed      Thu    > Fri <|");
         }
 
@@ -77,40 +77,40 @@ fn main() {
             .time_delay(Duration::from_millis(20))
             .build();
 
-        println!(""); //I did the new lines like this to make it more readable for me.
+         // println!(""); //I did the new lines like this to make it more readable for me.
 
 
         if weekdate == "Mon" {
             //I've separated the lines into 3 prints so that it lines up here, I could turn it all into one print tho.
             printer.print("   5 days till the weekend, good luck...");
-            printer.print("---------------------------------------------");
+            printer.print("---------------------------------------------------");
             printer.print("|> Mon <    Tue      Wed      Thu      Fri  |");
         }
 
         if weekdate == "Tue" {
             printer.print("    4 days till the weekend, keep going!");
-            printer.print("---------------------------------------------");
+            printer.print("---------------------------------------------------");
             printer.print("|  Mon    > Tue <    Wed      Thu      Fri  |");
         }
 
         if weekdate == "Wed" {
             
             printer.print("   3 days till the weekend, half way there!");
-            printer.print("---------------------------------------------");
+            printer.print("---------------------------------------------------");
             printer.print("|  Mon      Tue    > Wed <    Thu      Fri  |");
         }
 
         if weekdate == "Thu" {
             
             printer.print("    2 days till the weekend, almost there");
-            printer.print("---------------------------------------------");
+            printer.print("---------------------------------------------------");
             printer.print("|  Mon      Tue      Wed    > Thu <    Fri  |");
         }
 
         if weekdate == "Fri" {
             
             printer.print("1 day till the weekend, last day, u got this!");
-            printer.print("---------------------------------------------");
+            printer.print("---------------------------------------------------");
             printer.print("|  Mon      Tue      Wed      Thu    > Fri <|");
         }
 
